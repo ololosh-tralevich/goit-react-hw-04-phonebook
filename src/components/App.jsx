aaf7a7b5d4cfa33e77b433468523e4dfdd447854
ololosh-tralevich <karpeko76@gmail.com>
2022-03-31 21:02:14 +0100

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,69 @@
-import { Component } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { nanoid } from 'nanoid';
 
-import Phonebook from './phonebook/Phonebook';
+import ContactForm from './contactForm/ContactForm';
+import Filter from './filter/Filter';
+import ContactList from './contactList/ContactList';
 
-export class App extends Component {
-  render() {
-    return <Phonebook />;
-  }
-}
+import styles from './app.module.css';
+
+export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  const firstRender = useRef(true);
+
+  useEffect(() => {
+    const localContacts = JSON.parse(localStorage.getItem('contacts'));
+    if (firstRender.current && localContacts.length) {
+      setContacts(localContacts);
+    }
+    if (!firstRender.current && localContacts.length !== contacts.length) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+    firstRender.current = false;
+  }, [contacts]);
+
+  const filterContacts = ev => {
+    setFilter(ev.target.value);
+  };
+
+  const addContactBtn = contactData => {
+    const { name, number } = contactData;
+    const clone = contacts.find(
+      clone => clone.name === name || clone.number === number
+    );
+
+    clone
+      ? alert(`${name} is already in your contacts`)
+      : setContacts(prevState => {
+          return [...prevState, { id: nanoid(), name: name, number: number }];
+        });
+  };
+
+  const deleteContactBtn = id => {
+    setContacts(prevState => {
+      return [...prevState.filter(contact => contact.id !== id)];
+    });
+  };
+
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.contactContainer}>
+        <ContactForm onSubmit={addContactBtn} />
+      </div>
+
+      <div className={styles.listContainer}>
+        <h2>Contacts</h2>
+        <Filter filterContacts={filterContacts} filter={filter} />
+        <ContactList
+          deleteContactBtn={deleteContactBtn}
+          contacts={contacts}
+          filter={filter}
+        />
+      </div>
+    </div>
+  );
+};
+
+// export App;
